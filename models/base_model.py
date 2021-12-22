a3e11df9d83276cbf94d9e87f96771575f46903c
Sandra Hoyos <slhoyosr@gmail.com>
2021-12-22 11:53:00 -0500

    Update base_model.py
    base model

--- models/base_model.py
+++ models/base_model.py
@@ -19,25 +19,21 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
-        if not kwargs:
+        if kwargs:
+            for name, value in kwargs.items():
+                if name != '__class__':
+                    if name == 'created' or name == 'update_at':
+                        value = datetime.strptime(
+                            value,"%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, name, value)
+            if 'id' not in kwargs:
+                setattr(self 'id', str(uuid.uuid4()))
+
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-        else:
-            for attr in ['updated_at', 'created_at']:
-                if attr in kwargs:
-                    kwargs[attr] = datetime.strptime(
-                        kwargs[attr], '%Y-%m-%dT%H:%M:%S.%f')
-                else:
-                    kwargs[attr] = datetime.now()
-
-            if 'id' not in kwargs:
-                kwargs['id'] = str(uuid.uuid4())
-
-            if '__class__' in kwargs:
-                del kwargs['__class__']
-
-            self.__dict__.update(kwargs)
+        
+        
 
     def __str__(self):
         """Returns a string representation of the instance"""
