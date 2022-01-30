16b45424b42cde2b3b3b8463ded08f3fbf86adca
paulacarvani <3312@holbertonschool.com>
2022-01-29 22:20:50 -0800

    Fixed pep8, script tested

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -26,7 +26,7 @@ def C_text(text):
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_text(text = 'is cool'):
+def python_text(text='is cool'):
     return "Python {}".format(text.replace('_', ' '))
 
 if __name__ == '__main__':
