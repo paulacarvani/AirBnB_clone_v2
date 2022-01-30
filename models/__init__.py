7587408e23f0883695bee15fff7cd0beee5a20ff
paulacarvani <3312@holbertonschool.com>
2022-01-30 16:45:56 -0500

    Changing files

--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""
+initialize the models package
+"""
+
 from os import getenv
 
-
+# DBSTORAGE
 if getenv('HBNB_TYPE_STORAGE') == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
+# FILESTORAGE
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
