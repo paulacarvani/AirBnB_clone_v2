d4dac86f65f31134acebb5b0b6682f6163e1f415
paulacarvani <3312@holbertonschool.com>
2022-01-30 22:12:36 -0500

    Correction of task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-"""Flask app to generate html list of all states from storage"""
+"""starts a Flask web application"""
+
 from flask import Flask, render_template
-from models import *
 from models import storage
+from models.state import State
+
+
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """display a HTML page with the states listed in alphabetical order"""
-    states = storage.all("State").values()
-    return render_template('7-states_list.html', states=states)
-
-
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
+def close(self):
+    """Closes connection with db"""
     storage.close()
 
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """Returns a dictionary of state objects"""
+    return render_template("7-states_list.html", states=storage.all(State))
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host="0.0.0.0", port="5000")
